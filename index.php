c85ebec86894dd5c1329b07d93e09a9c0fd6128b
Kavaikun <152216424+Kavaikun@users.noreply.github.com>
2024-10-24 18:59:18 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+// Подключение файла соединения с БД
+include_once 'db.class.php';
+
+// хост БД
+define('db_host','localhost');
+
+// Имя БД
+define('db_name','tasks');
+
+// Пользователь БД
+define('db_user','admin');
+
+// Пароль БД
+define('db_pass','admin_password');
+
+// Обявление класса для подключения к бд
+$db = new DB_class(db_host,db_name,db_user,db_pass);
+
+$query = $db->select(true,"*","ZADACHI","task_id='1'");
+
+print_r($query);
+
+
+
+?>

7a18ccf376419f9d84ce220ab96961f6fb7ae1a1
Kavaikun <152216424+Kavaikun@users.noreply.github.com>
2024-10-26 11:05:31 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,22 +2,25 @@
 // Подключение файла соединения с БД
 include_once 'db.class.php';
 
-// // хост БД
-// define('db_host','localhost');
+// хост БД
+define('db_host','localhost');
 
-// // Имя БД
-// define('db_name','testsmao');
+// Имя БД
+define('db_name','testsmao');
 
-// // Пользователь БД
-// define('db_user','admin');
+// Пользователь БД
+define('db_user','admin');
 
-// // Пароль БД
-// define('db_pass','admin_password');
+// Пароль БД
+define('db_pass','admin_password');
 
-// // Обявление класса для подключения к бд
-// $db = new DB_class(db_host,db_name,db_user,db_pass);
+// Обявление класса для подключения к бд
+$db = new DB_class(db_host,db_name,db_user,db_pass);
 
 // $query = $db->select(true,"*","tasks","task_id='1'");
 
 // print_r($query);
+
+
+
 ?>

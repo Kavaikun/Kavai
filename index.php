41fb68db87e3936ffcfd39e3baa85fc64916fa48
Kavaikun <152216424+Kavaikun@users.noreply.github.com>
2024-10-26 11:23:25 +0300

    Update index.php

--- index.php
+++ index.php
@@ -19,8 +19,64 @@ $db = new DB_class(db_host,db_name,db_user,db_pass);
 
 $query = $db->select(true,"*","tasks","task_id='1'");
 
-print_r($query);
-
 
 
 ?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Задачник который надеюсь будет работать</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="tasks">
+	<div class="task">
+		<div class="task-control">
+			<input type="checkbox" name="">
+		</div>
+		<div class="task-text">Проснуться утром</div>
+		<div class="task-date">
+			<input type="date" name="">
+		</div>		
+	</div>
+		<div class="task">
+		<div class="task-control">
+			<input type="checkbox" name="">
+		</div>
+		<div class="task-text">Открыть глаза</div>
+		<div class="task-date">
+			<input type="date" name="">
+		</div>		
+	</div>
+		<div class="task">
+		<div class="task-control">
+			<input type="checkbox" name="">
+		</div>
+		<div class="task-text">Встать с кровати</div>
+		<div class="task-date">
+			<input type="date" name="">
+		</div>		
+	</div>
+		<div class="task">
+		<div class="task-control">
+			<input type="checkbox" name="">
+		</div>
+		<div class="task-text">Сьесть чтото съедобое</div>
+		<div class="task-date">
+			<input type="date" name="">
+		</div>		
+	</div>
+		<div class="task">
+		<div class="task-control">
+			<input type="checkbox" name="">
+		</div>
+		<div class="task-text">Включить комп</div>
+		<div class="task-date">
+			<input type="date" name="">
+		</div>		
+</div>
+</body>
+</html>
